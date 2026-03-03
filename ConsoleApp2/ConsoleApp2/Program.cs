7d4c8dddbc14b4acee8e5041a1737cbd85ef5235
hemzebagirov2006-crypto <hemzebagirov2006@gmail.com>
2026-03-03 16:59:48 +0400

    fayl gonderilmisdir

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 5;
+            int cem = a + b;
+            int ferq = a - b;
+            int hasil = a * b;
+            int nisbet = a / b;
+            int qaliq = a % b;
+
+     
+            Console.WriteLine($"Cem={cem},Ferq={ferq},Hasil={hasil},Nisbet={nisbet},qaliq={qaliq}");
+        }
+    }
+}
